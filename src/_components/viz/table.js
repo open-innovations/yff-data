0a6840f702b14280cfad1e736a3411936d0ac06c
Giles Dring <giles@dringtech.com>
2022-07-25 18:07:22 +0100

    Add heatmap style

--- src/_components/viz/table.js
+++ src/_components/viz/table.js
@@ -1,3 +1,5 @@
+import { Colours, Colour } from '/oi/colour.js';
+
 export const css = `
   table { border-collapse: collapse; width: 100%; }
   th { border: 0; font-weight: bold; }
@@ -6,6 +8,14 @@ export const css = `
   td:last-child { border-right: 0; }
   td, th { padding: 1rem; }
 `;
+
+const colourScales = new Colours({
+  'Viridis': 'rgb(122,76,139) 0%, rgb(124,109,168) 12.5%, rgb(115,138,177) 25%, rgb(107,164,178) 37.5%, rgb(104,188,170) 50%, rgb(133,211,146) 62.5%, rgb(189,229,97) 75%, rgb(254,240,65) 87.5%, rgb(254,240,65) 100%',
+  'Heat': 'rgb(0,0,0) 0%, rgb(128,0,0) 25%, rgb(255,128,0) 50%, rgb(255,255,128) 75%, rgb(255,255,255) 100%',
+  'Planck': 'rgb(0,0,255) 0%, rgb(0,112,255) 16.666%, rgb(0,221,255) 33.3333%, rgb(255,237,217) 50%, rgb(255,180,0) 66.666%, rgb(255,75,0) 100%',
+  'Plasma': 'rgb(12,7,134) 0%, rgb(82,1,163) 12.5%, rgb(137,8,165) 25%, rgb(184,50,137) 37.5%, rgb(218,90,104) 50%, rgb(243,135,72) 62.5%, rgb(253,187,43) 75%, rgb(239,248,33) 87.5%',
+  'YFF': 'rgb(99,190,123) 0%, rgb(250,233,131) 50%, rgb(248,105,107) 100%'
+});
 
 export default ({ config, sources }) => {
   // Get loaded data from sources object
@@ -40,7 +50,7 @@ export default ({ config, sources }) => {
   }
   html.push('</thead>');
 
-  const mergeRows = table.names.reduce((acc, key) => ({ ...acc, [key]: config.column?.find(x => x.name === key)?.mergeRows || false }), {});
+  const mergeRows = table.names.reduce((acc, key) => ({ ...acc, [key]: config.columns?.find(x => x.name === key)?.mergeRows || false }), {});
 
   // Build data part
   html.push('<tbody>')
@@ -62,7 +72,21 @@ export default ({ config, sources }) => {
           output = false;
         }
       };
-      if (output) html.push(`<td${count ? ' rowspan=' + count : ''}>${cell}</td>`)
+      const rowSpan = count ? 'rowspan=' + count : '';
+      const cellText = (isNaN(cell) && ['float'].includes(table.types[colNumber])) ? '' : cell;
+      const { align, heatmap, scale = 'Viridis', min, max } = config.columns?.find(x => x.name === colName) || {};
+      let heatMapStyle = '';
+      console.log(typeof cell === 'number')
+      if (heatmap && !isNaN(cell)) {
+        const background = colourScales.getColourFromScale(scale, cell, min, max);
+        const textCol = (new Colour(background)).text;
+        heatMapStyle = `background: ${background}; color: ${textCol};`;
+      }
+      const style = `style="
+      ${ align ? 'text-align: ' + align + ';' : ''}
+      ${ heatMapStyle }
+      "`;
+      if (output) html.push(`<td ${rowSpan} ${style}>${cellText}</td>`)
     }
     html.push('</tr>');
   }
