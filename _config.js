86e450322664d46c78d863679c97e712fd8376c6
Giles Dring <giles@dringtech.com>
2022-08-10 14:29:03 +0100

    Tidying up the config file

--- _config.js
+++ _config.js
@@ -1,24 +1,24 @@
-import lume from "lume/mod.ts";
+import lume from 'lume/mod.ts';
 
-import jsonLoader from "lume/core/loaders/json.ts";
-import basePath from "lume/plugins/base_path.ts";
+import jsonLoader from 'lume/core/loaders/json.ts';
+import basePath from 'lume/plugins/base_path.ts';
 import esbuild from 'lume/plugins/esbuild.ts';
-import netlifyCMS from "lume/plugins/netlify_cms.ts";
+import netlifyCMS from 'lume/plugins/netlify_cms.ts';
 // import postcss from "lume/plugins/postcss.ts";
-import resolveUrls from "lume/plugins/resolve_urls.ts";
-import slugifyUrls from "lume/plugins/slugify_urls.ts";
-import { stringify as yamlStringify } from "std/encoding/yaml.ts";
-import { copy } from "std/fs/copy.ts";
+import resolveUrls from 'lume/plugins/resolve_urls.ts';
+import slugifyUrls from 'lume/plugins/slugify_urls.ts';
+import { stringify as yamlStringify } from 'std/encoding/yaml.ts';
+import { copy } from 'std/fs/copy.ts';
 import csvLoader from '/src/_lib/oi/csv-loader.ts';
 import autoDependency from '/src/_lib/oi/auto-dependency.ts';
 
 const site = lume({
   src: './src',
-  location: new URL("https://open-innovations.github.io/yff-wireframe/"),
+  location: new URL('https://open-innovations.github.io/yff-wireframe/'),
 });
 
 // Also process .html files
-site.loadPages(['.html'])
+site.loadPages(['.html']);
 
 site.use(basePath());
 site.use(resolveUrls());
@@ -39,40 +39,42 @@ site.copy(['.svg']);
 site.copy(['.png']);
 
 // Process Javascript files
-site.use(esbuild({
-  extensions: ['.esbuild.js'],
-  options: {
-    bundle: true,
-    format: "iife",
-    minify: true,
-    keepNames: true,
-    platform: "browser",
-    target: "es6",
-    incremental: true,
-    treeShaking: true,
-  }
-}));
+site.use(
+  esbuild({
+    extensions: ['.esbuild.js'],
+    options: {
+      bundle: true,
+      format: 'iife',
+      minify: true,
+      keepNames: true,
+      platform: 'browser',
+      target: 'es6',
+      incremental: true,
+      treeShaking: true,
+    },
+  })
+);
 
 // Add csv loader
-site.loadData([".csv"], csvLoader);
-site.loadData([".geojson", ".hexjson"], jsonLoader);
+site.loadData(['.csv'], csvLoader);
+site.loadData(['.geojson', '.hexjson'], jsonLoader);
 
 // Copy source data files to live site
 const interimDataDir = 'generated-data-directory';
-site.script("copy-data-files", async () => {
-  await copy("src/_data/sources", `src/${interimDataDir}/`, {
+site.script('copy-data-files', async () => {
+  await copy('src/_data/sources', `src/${interimDataDir}/`, {
     overwrite: true,
   });
 });
 
 // Execute it before the site is built
-site.addEventListener("beforeBuild", "copy-data-files");
+site.addEventListener('beforeBuild', 'copy-data-files');
 
 site.copy(interimDataDir, 'data');
 
 site.process(['.html'], autoDependency);
 
 // Add filters
-site.filter("yaml", (value, options = {}) => yamlStringify(value, options));
+site.filter('yaml', (value, options = {}) => yamlStringify(value, options));
 
 export default site;
