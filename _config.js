0c64243e2fb12b9a48a6e19f0d98b406767953a5
slowe <299787+slowe@users.noreply.github.com>
2022-08-23 13:04:49 +0100

    Keep link content in `striplinks` filter

--- _config.js
+++ _config.js
@@ -79,7 +79,7 @@ site.process(['.html'], autoDependency);
 
 // Add filters
 site.filter('yaml', (value, options = {}) => yamlStringify(value, options));
-site.filter('striplinks', (value) => value.replace(/<a\b[^>]*>/gi).replace(/<\/a>/gi) );
+site.filter('striplinks', (value) => value.replace(/<a\b[^>]*>([^\<]*)<\/a>/gi,function(m,p1){ return p1; }) );
 
 // URL re-writing plugins. These have to be last to enable any urls installed by the
 // processors to be re-written
