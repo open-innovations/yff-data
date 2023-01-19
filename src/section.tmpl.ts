6f309d672530564fa5a08865e811d5dbeb01b5d8
Giles Dring <giles@dringtech.com>
2023-01-19 17:02:50 +0000

    Creating report structure outside data config

---
+++ src/section.tmpl.ts
@@ -0,0 +1,27 @@
+export const layout ='section.njk';
+export const tags = ['section'];
+
+export default function*({ search }) {
+  for (const report of search.pages('report')) {
+    const {
+      title: reportTitle,
+      url,
+      sections = [],
+    } = report.data;
+
+    // Iterate through the sections
+    for (const section of sections) {
+      const {
+        title,
+        read_more,
+      } = section;
+
+      if (read_more === undefined || read_more !== false) yield {
+        url: `${url}${title}/`,
+        reporttitle: `${reportTitle}`,
+        report: url,
+        ...section,
+      }
+    }  
+  }
+}
