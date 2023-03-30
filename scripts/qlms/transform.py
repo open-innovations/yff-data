432da805c98ea8cc6ccfb1e103a5d3ea5867e083
Giles Dring <giles@dringtech.com>
2023-03-30 17:34:44 +0100

    Adding extract of 16 to 24 not in education
    Updates #19

--- scripts/qlms/transform.py
+++ scripts/qlms/transform.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python
 import os
 import pandas as pd
-from extract import QLMS_RAW_LATEST
+from extract import A06_SA_LATEST
 
 
 DATA_DIR = os.path.realpath(os.path.join('data', 'qlms'))
 os.makedirs(DATA_DIR, exist_ok=True)
 
 
-def main():
-    data9 = pd.read_excel(QLMS_RAW_LATEST, sheet_name='9',
-                          skiprows=4, header=3)
-    data9 = data9[~data9[data9.columns[1]].isna()].reset_index(drop=True)
-    data9.rename(columns={
-        'Dataset identifier code': 'Quarter'
-    }, inplace=True)
-    data9.to_csv(os.path.join(DATA_DIR, 'qlms-9.csv'), index=False)
-
-    data11 = pd.read_excel(QLMS_RAW_LATEST, sheet_name='11',
-                           skiprows=4, header=3, na_values='..')
-    data11 = data11[~data11[data11.columns[1]].isna()].reset_index(drop=True)
-    data11.rename(columns={
-        'Dataset identifier code': 'Quarter'
-    }, inplace=True)
-    data11.to_csv(os.path.join(DATA_DIR, 'qlms-11.csv'), index=False)
-
-    data12 = pd.read_excel(QLMS_RAW_LATEST, sheet_name='12',
-                           skiprows=4, header=3, na_values='..')
-    data12 = data12[~data12[data12.columns[1]].isna()].reset_index(drop=True)
-    data12.rename(columns={
-        'Dataset identifier code': 'Quarter'
-    }, inplace=True)
-    data12.to_csv(os.path.join(DATA_DIR, 'qlms-12.csv'), index=False)
-
-    data20 = pd.read_excel(QLMS_RAW_LATEST, sheet_name='20',
-                           skiprows=4, header=3, usecols="A:E",
-                           na_values='..')
-    data20 = data20[~data20[data20.columns[2]].isna()].reset_index(drop=True)
-    data20.rename(columns={
-        'Unnamed: 0': 'Quarter'
-    }, inplace=True)
-    data20.to_csv(os.path.join(DATA_DIR, 'qlms-20.csv'), index=False)
-
-
 if __name__ == "__main__":
-    main()
+    # Read the latest A06 data
+    A06_data = pd.read_excel(A06_SA_LATEST,
+                             sheet_name='People',
+                             skiprows=4,
+                             header=[0, 1, 2, 3],
+                             index_col=0)
+    # Drop any rows with nothin in columns 1
+    A06_data = A06_data[A06_data.iloc[:, 0].notna()]
+    A06_data.index.names = ['quarter']
+
+    # Reshape the index...
+    # Drop the dataset identifier code
+    A06_data.columns = A06_data.columns.droplevel(level=[2, 3])
+
+    # Get just quarters
+    A06_data = A06_data.loc[A06_data.index.str.slice(
+        stop=3).isin(['Jan', 'Apr', 'Jul', 'Oct'])]
+
+    A06_data.loc[:, ('All aged 16 to 24 not in full-time education 1')] \
+      .rename(columns=lambda x: x.lower().replace(' ', '_')) \
+      .to_csv(os.path.join(DATA_DIR, '16_to_24_not_in_education.csv'))
