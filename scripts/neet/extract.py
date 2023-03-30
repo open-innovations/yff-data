71a82604fee46f88dd9b701dc88963459c7d78a6
Giles Dring <giles@dringtech.com>
2023-03-30 16:37:27 +0100

    Add initial NEET pipline
    Updates #20

--- scripts/neet/extract.py
+++ scripts/neet/extract.py
@@ -7,10 +7,11 @@ WORK_DIR = os.path.join('working', 'neet')
 os.makedirs(WORK_DIR, exist_ok=True)
 NEET_RAW_LATEST = os.path.join(WORK_DIR, 'neetlatest.xlsx')
 
+NEET_DATA_SOURCE = 'https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment/datasets/youngpeoplenotineducationemploymentortrainingneettable1'
+
 
 def download_latest():
-    url = get_filename(
-        'https://www.ons.gov.uk/employmentandlabourmarket/peoplenotinwork/unemployment/datasets/youngpeoplenotineducationemploymentortrainingneettable1', pattern='.xlsx$')
+    url = get_filename(NEET_DATA_SOURCE, pattern='.xlsx$')
     download_file(url, NEET_RAW_LATEST)
 
 
---
+++ scripts/neet/transform.py
@@ -0,0 +1,59 @@
+import os
+import pandas as pd
+
+from extract import NEET_RAW_LATEST
+
+DATA_DIR = os.path.join('data', 'neet')
+os.makedirs(DATA_DIR, exist_ok=True)
+
+NEET_16_24 = os.path.join(DATA_DIR, 'neet_16_to_24.csv')
+
+
+def load_data():
+    # Load the relevant sheef from the excel file
+    data = pd.read_excel(NEET_RAW_LATEST,
+                         sheet_name='People - SA',
+                         skiprows=4,
+                         header=[0, 1, 2, 3],
+                         index_col=0,
+                         na_values=['..'])
+
+    # Remove any rows with no information in the first column
+    data = data[data.iloc[:, 0].notna()]
+
+    data.index.names = ['quarter']
+
+    # Return the data
+    return data
+
+
+def reindex_columns(data):
+    new_index = data.columns.to_frame()
+
+    # Set sensible names for levels
+    new_index.columns = ['age_range', 'group', 'measure', 'numeric']
+
+    # Rename measures to make them easier to deal with
+    new_index.loc[new_index.group == 'Young people who were NEET',
+                  ['measure']] = 'neet_' + new_index.measure.str.lower().str.replace(r"\s+", "_", regex=True)
+    new_index.loc[new_index.group == 'Total people in relevant population group',
+                  ['measure']] = 'total_popuplation'
+    new_index.loc[new_index.group == 'People who were NEET as a percentage of people in relevant population group',
+                  ['measure']] = 'percentage_neet'
+
+    # Drop levels 'group' and 'numeric'
+    new_index.drop(columns=['group', 'numeric'], inplace=True)
+
+    # Convert back into an index and reassign to dataframe
+    data.columns = pd.MultiIndex.from_frame(new_index)
+
+    # Return the data
+    return data
+
+
+if __name__ == "__main__":
+    data = load_data()
+    data = reindex_columns(data)
+
+    # Select just the 16-24 age group and save to CSV
+    data.loc[:, ('Aged 16-24')].to_csv(NEET_16_24)
