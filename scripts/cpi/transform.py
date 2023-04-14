ec0c106d6e732b597f8095492c04ffe87b295c8d
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-04-14 16:33:30 +0100

    Update CPI pipeline

--- scripts/cpi/transform.py
+++ scripts/cpi/transform.py
@@ -17,8 +17,9 @@ def load_data(filename, sheet_name, header=None, index_col=None):
     return data
 
 if __name__ == "__main__":
-    data = load_data(CPI_LATEST, sheet_name='Table 4', header=[4, 8, 9], index_col=0)
-    measures = ['CHZR', 'CHZT', 'CHZU', 'CHZZ']
+    data = load_data(CPI_LATEST, sheet_name='Table 4', header=[4, 8, 9], index_col=1)
+
+    measures = ['D7BU', 'D7BW', 'D7BX', 'D7C4']
     columns = [' Percentage change', 'Percentage change over 12 months']
     
     CPI_data = data.loc[measures]
@@ -28,25 +29,35 @@ if __name__ == "__main__":
     # most recent month pct change data.
     pct_change_12_months = CPI_data['Percentage change over 12 months'].iloc[:, -1:]
     pct_change_1_month = CPI_data[' Percentage change'].iloc[:, -2:-1]
-    #print(pct_change_12_months)
-    #rint(pct_change_1_month)
+    
+    #merge and rename the two frames
     df = pct_change_1_month.merge(pct_change_12_months, left_index=True, right_index=True)
     df.rename(columns={'Unnamed: 12_level_2': 'pct_change_1_month', 'Unnamed: 22_level_2': 'pct_change_12_months'}, inplace=True)
-    #print(df.droplevel(df.columns[0]))
+
+    #select only the values we need.
     month = df.columns[0][0]
     df = df[month]
-    df.to_csv(os.path.join(DATA_DIR, 'CPI.csv'))
+    
 
     #data2 = load_data(CPI_LATEST, sheet_name='Table 21', header=[5,6], index_col=2)
     data2 = pd.read_excel(CPI_LATEST, sheet_name='Table 21', index_col=1, skiprows=4, header=[0,1])
-    measures2 = ['D7BU', 'D7BW', 'D7BX', 'D7C4']
-    table21 = data2.loc[measures2]
+    table21 = data2.loc[measures]
     most_recent_month = table21.iloc[:, -1:]
     quarter = table21.iloc[:, -4:-3]
-    most_recent_month.astype('float', errors='ignore')
-    quarter.astype('float', errors='ignore')
-    quarterly_change = most_recent_month.subtract(quarter, fill_value=None)
-    print(quarterly_change)
-
+    
+    m1 = most_recent_month.columns[0][1]
+    m2 = quarter.columns[0][1]
+    
+    most_recent_month.rename(columns={m1: 'quarterly_idx_change'}, inplace=True)
+    quarter.rename(columns={m2: 'quarterly_idx_change'}, inplace=True)
+    quarterly_change = most_recent_month[most_recent_month.columns[0][0]] - quarter[quarter.columns[0][0]]
+   
+    df['quarterly_idx_change'] = quarterly_change
+    column_name = pd.read_csv('working/lookups/MM23_variable_lookup.csv',
+                          usecols=['code', 'name'], index_col='code').to_dict()['name']
+    
+    df.rename(index=column_name, inplace=True)
+    df.to_csv(os.path.join(DATA_DIR, 'CPI.csv'))
+    
     
     
