eb0294b3a76eb875446056c4bb2953f1c667a53b
Giles Dring <giles@dringtech.com>
2023-04-19 17:53:06 +0100

    Create basic topic layout
    Closes #15

--- src/topic/topic.tmpl.ts
+++ src/topic/topic.tmpl.ts
@@ -2,6 +2,8 @@ const firstCharacterInSentence = /(^\w{1}|\.\s*\w{1})/gi;
 const firstCharacter = /\w/;
 const followingSpaces = /\b\w/g
 
+export const layout = 'layouts/topic.njk';
+
 export default function*({ search }) {
   const topics = (search.values('topics') as string[]).map(s => s.trim());
 
