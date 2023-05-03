4c5f0ba92c93a1a3715c7307cf921dfd2dc0d8e5
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2023-05-03 15:44:08 +0100

    Update vacancies pipeline

--- scripts/vacancies/extract.py
+++ scripts/vacancies/extract.py
@@ -12,7 +12,5 @@ VACANCIES_URL = 'https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/e
 if __name__ == '__main__':
     url = get_link_by_text(VACANCIES_URL, pattern=r'Vacancies and jobs in the UK')
 
-    # Pulling latest URL from a list is fragile as the page layout may change. TODO: Alternative way to find urls
-    download_latest(url, os.path.join(WORK_DIR, 'vacancies.csv'), pattern='&format=csv$')
     download_latest(url, os.path.join(WORK_DIR, 'vacancies_by_sector.csv'), pattern='&format=csv$', index=1)
 
--- scripts/vacancies/prepare.py
+++ scripts/vacancies/prepare.py
@@ -1,19 +1,24 @@
-import os
-import pandas as pd
+import os 
+import pandas as pd 
 
+
+DATA_DIR = os.path.join('src', '_data', 'sources', 'vacancies')
 RAW_DATA_DIR = os.path.realpath(os.path.join('data', 'vacancies'))
 WORK_DIR = os.path.join('working', 'vacancies')
 
-
-DATA_DIR = os.path.realpath(os.path.join('src', '_data', 'sources', 'vacancies'))
-os.makedirs(DATA_DIR, exist_ok=True)
-
 def prepare_vacancies(): 
-    vacancies = pd.read_csv(os.path.join(WORK_DIR, 'vacancies.csv'), skiprows=6)
-    vacancies = vacancies.reset_index().rename(columns = { vacancies.index.name: 'Index'})
-    vacancies.to_csv(os.path.join(DATA_DIR, 'vacancies.csv'), index = False)
+    vacancies = pd.read_csv(os.path.join(RAW_DATA_DIR, 'vacancies_edd.csv'), index_col= ['index'])
+    vacancies.sort_values(by='date', ascending = True, inplace = True) 
 
-def prepare_vacancies_by_sector():
+    quarterly = vacancies.loc[vacancies['freq'] == 'q'].reset_index()
+    quarterly = quarterly.drop(columns= ['index', 'freq'], axis = 1).reset_index()
+    quarterly.to_csv(os.path.join(DATA_DIR, 'quarterly_vacancies.csv'), index=False)
+
+    monthly = vacancies.loc[vacancies['freq'] == 'q'].reset_index()
+    monthly = monthly.drop(columns= ['index', 'freq'], axis = 1).reset_index()
+    monthly.to_csv(os.path.join(DATA_DIR, 'monthly_vacancies.csv'), index=False)
+
+def prepare_vacancies_by_sector(): 
     vacancies_by_sector = pd.read_csv(os.path.join(WORK_DIR, 'vacancies_by_sector.csv'), skiprows = 6)
     vacancies_by_sector = vacancies_by_sector.rename(columns = 
         { vacancies_by_sector.columns[0]: 'Sector',
@@ -22,6 +27,7 @@ def prepare_vacancies_by_sector():
         })
     vacancies_by_sector['Sector'] = vacancies_by_sector['Sector'].str.replace('<br>', '', regex=True).str.wrap(25)
     vacancies_by_sector.to_csv(os.path.join(DATA_DIR, 'vacancies_by_sector.csv'), index = False)
+
 
 if __name__ == "__main__":
     prepare_vacancies()
