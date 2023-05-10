9a80a870d0b0cc7628b29e24b733eb25d610f64e
Giles Dring <giles@dringtech.com>
2023-05-10 13:43:05 +0100

    Moving QLFS summarise script to prepare script

--- scripts/qlfs/prepare.py
+++ scripts/qlfs/prepare.py
@@ -1,10 +1,29 @@
 import os
 import scripts.util.file
+import pandas as pd
 
 from transform import DATA_DIR as RAW_DATA_DIR
 
-DATA_DIR = 'src/_data/sources/qlfs'
+DATA_DIR = os.path.realpath(os.path.join('src', '_data', 'sources', 'qlfs'))
 os.makedirs(DATA_DIR, exist_ok=True)
+
+def summarise(): 
+    long_term_unemployed = pd.read_csv(os.path.join(DATA_DIR, 'long_term_unemployed.csv'))
+    not_in_education = pd.read_csv(os.path.join(DATA_DIR, 'not_in_education.csv'))
+
+    summary = pd.DataFrame({
+            'Unemployment rate' : (not_in_education.age_16_to_24_not_in_ft_education_unemployment_rate_sa.tail(1)).round(1),
+            'Long-term unemployment rate':(long_term_unemployed.age_16_to_24_unemployed_over_12_months_rate_sa.tail(1)).round(1),
+            'Economic inactivity rate':(not_in_education.age_16_to_24_not_in_ft_education_economic_inactivity_rate_sa.tail(1)).round(1)
+    }).T.reset_index()
+    summary = summary.rename(columns = {'index': 'Title', 11: 'Value'})
+    summary['Note'] = [
+        "Young people aged 16-24, not in full-time education (seasonally adjusted)",
+        "Young people aged 16-24, unemployed over 12 months (seasonally adjusted)",
+        "Young people age 16-24 not in full-time education, economically inactive (seasonally adjusted)"
+        ]
+    summary['Suffix'] = '%'
+    summary.to_csv(os.path.join(DATA_DIR, 'headlines.csv'), index=False)
 
 
 if __name__ == "__main__":
@@ -12,3 +31,4 @@ if __name__ == "__main__":
                    from_dir=RAW_DATA_DIR, to_dir=DATA_DIR)
     scripts.util.file.copy("not_in_education.csv",
                    from_dir=RAW_DATA_DIR, to_dir=DATA_DIR)
+    summarise()
--- scripts/qlfs/transform.py
+++ scripts/qlfs/transform.py
@@ -6,7 +6,6 @@ from extract import A06_SA_LATEST, UNEM01_SA_LATEST
 from scripts.util.date import quarter_to_date, most_recent_stats
 
 DATA_DIR = os.path.realpath(os.path.join('data', 'qlfs'))
-SUMMARY_DIR = os.path.realpath(os.path.join('src', '_data', 'sources', 'qlfs'))
 os.makedirs(DATA_DIR, exist_ok=True)
 
 column_name = pd.read_csv('working/lookups/lms_lookups.csv',
@@ -97,28 +96,10 @@ def transform_UNEM01():
 
     return UNEM01_data
 
-def summarise(): 
-    long_term_unemployed = pd.read_csv(os.path.join(DATA_DIR, 'long_term_unemployed.csv'))
-    not_in_education = pd.read_csv(os.path.join(DATA_DIR, 'not_in_education.csv'))
-
-    summary = pd.DataFrame({
-            'Unemployment rate' : (not_in_education.age_16_to_24_not_in_ft_education_unemployment_rate_sa.tail(1)).round(1),
-            'Long-term unemployment rate':(long_term_unemployed.age_16_to_24_unemployed_over_12_months_rate_sa.tail(1)).round(1),
-            'Economic inactivity rate':(not_in_education.age_16_to_24_not_in_ft_education_economic_inactivity_rate_sa.tail(1)).round(1)
-    }).T.reset_index()
-    summary = summary.rename(columns = {'index': 'Title', 11: 'Value'})
-    summary['Note'] = [
-        "Young people aged 16-24, not in full-time education (seasonally adjusted)",
-        "Young people aged 16-24, unemployed over 12 months (seasonally adjusted)",
-        "Young people age 16-24 not in full-time education, economically inactive (seasonally adjusted)"
-        ]
-    summary['Suffix'] = '%'
-    summary.to_csv(os.path.join(SUMMARY_DIR, 'headlines.csv'), index=False)
 
 if __name__ == "__main__":
     transform_A06()
     transform_UNEM01()
-    summarise()
 
     notes = [
         'Long-term unemoployment levels for 16-24 are derived from aggregates 16-17 and 18-24 rates',
