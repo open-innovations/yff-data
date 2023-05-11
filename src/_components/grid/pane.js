1c8348cef1f3493f07d1633bb02bfcbbfb62e761
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2023-05-11 09:59:20 +0100

    Initial dashboard layout

--- src/_components/grid/pane.js
+++ src/_components/grid/pane.js
@@ -1,16 +1,18 @@
 export const css = `
 .auto-grid .pane {
   --max-height: unset;
-  padding: 1rem;
+  padding: 0rem;
   display: block;
   height: 100%;
-  min-height: 20rem;
+  min-height: 10rem;
   color: inherit;
+  background: #fceee7;  
+  text-align: left;
 }
 .auto-grid .pane:hover, .auto-grid .pane:focus { background: #efefef; }
 `;
 
-export default function ({ content, link, tag = 'div', maxHeight = undefined, extra_classes = 'grey-bg' }) {
+export default function ({ content, link, tag = 'div', maxHeight = undefined, extra_classes = 'grey-bg', rows = undefined, columns = undefined }) {
   // let paneContent = content;
   // if (link) {
   //   paneContent = `<a href=${link} class='>${content}</a>`
@@ -22,6 +24,12 @@ export default function ({ content, link, tag = 'div', maxHeight = undefined, ex
   let style = '';
   if (maxHeight !== undefined)
     style = `style="--max-height: ${maxHeight};"`
+
+  if (rows !== undefined)
+    style = `style="grid-row:auto/span ${ rows };"`
+
+  if (columns !== undefined)
+    style = `style="grid-column:span ${ columns };"`
 
   if ( link ) {
     return `
--- src/_components/viz/render.js
+++ src/_components/viz/render.js
@@ -2,7 +2,7 @@ import { applyReplacementFilters } from "/src/_lib/oi/util.js";
 
 export default function ({ comp, config, tab, title, caption, type }) {
   try {
-    const fileLink = config.file ? `<a href="${ config.file }" role="menuitem" class="figure-option orange-bg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-spreadsheet-fill" viewBox="0 0 16 16"><path d="M6 12v-2h3v2H6z"/><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM3 9h10v1h-3v2h3v1h-3v2H9v-2H6v2H5v-2H3v-1h2v-2H3V9z"/></svg> Download data</a>` : ''
+    const fileLink = config.file ? `<a href="${ config.file }" role="menuitem" class="figure-option orange-bg"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="12" fill="currentColor" class="bi bi-file-earmark-spreadsheet-fill" viewBox="0 0 16 16"><path d="M6 12v-2h3v2H6z"/><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM3 9h10v1h-3v2h3v1h-3v2H9v-2H6v2H5v-2H3v-1h2v-2H3V9z"/></svg> Download</a>` : ''
     const heading = title ? `<h3>${ title }</h3>` : ""
     const figCaption = caption ? `<figcaption><p>${ applyReplacementFilters(caption, config)}</p></figcaption>` : "";
     // {{ comp.viz[type]({ config: config }) | safe }}
