eaa4b782bf4b62da36d90f30347b21d9544e41fd
Giles Dring <giles@dringtech.com>
2023-05-24 12:50:18 +0100

    Release v2 of the site

--- _config.js
+++ _config.js
@@ -23,7 +23,7 @@ const site = lume({
 });
 
 // Change this to update the version of the site that is built. This mainly affects navigation.
-site.data('version', Deno.env.get('VERSION') || 'v1');
+site.data('version', Deno.env.get('VERSION') || 'v2');
 
 // To set the DEBUG global data, start the process with DEBUG=true in the environment
 if (Deno.env.get('DEBUG') !== undefined) site.data('DEBUG', true);
