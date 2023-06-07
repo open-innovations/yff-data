50ade2afdbe39d0900afbed688399850692a1b9f
Taz <tazmin.chiles@open-innovations.org>
2023-06-07 14:06:11 +0100

    Add vacancies charts for quarterly growth by sector

--- scripts/vacancies/prepare.py
+++ scripts/vacancies/prepare.py
@@ -24,19 +24,24 @@ def prepare_vacancies():
     monthly.to_csv(os.path.join(DATA_DIR, 'monthly_vacancies.csv'), index=False)
 
 def prepare_vacancies_by_sector(): 
-    vacancies_by_sector = pd.read_csv(os.path.join(WORK_DIR, 'vacancies_by_sector.csv'), skiprows = 6, index_col = 0)
-    vacancies_by_sector = vacancies_by_sector.drop(index = [
-        'Electricity gas steam & air conditioning supply', 
-        'Administrative & support service activities',
-        'Mining & quarrying',
-    ]).reset_index()
+    vacancies_by_sector = pd.read_csv(os.path.join(WORK_DIR, 'vacancies_by_sector.csv'), skiprows = 6)
+
     vacancies_by_sector = vacancies_by_sector.rename(columns = 
         { vacancies_by_sector.columns[0]: 'Sector',
         vacancies_by_sector.columns[1]: 'Growth compared with last quarter (%)',
         vacancies_by_sector.columns[2]: 'Growth compared with same quarter in 2020 (%)'
         })
-    vacancies_by_sector['Sector'] = vacancies_by_sector['Sector'].str.replace('<br>', '', regex=True).str.wrap(25)
-    vacancies_by_sector.to_csv(os.path.join(DATA_DIR, 'vacancies_by_sector.csv'), index = False)
+    vacancies_by_sector['Sector'] = vacancies_by_sector['Sector'].str.replace('<br>', '', regex=True)
+    vacancies_by_sector.to_csv(os.path.join(DATA_DIR, 'quarterly_growth_all_sectors.csv'), index = False)
+    
+    #TODO: Use sector names instead of index - order may change
+    key_youth_sectors = vacancies_by_sector.drop([0,1,2,3,5,7,8])
+    key_youth_sectors['Sector'] = key_youth_sectors['Sector'].str.wrap(25)
+    key_youth_sectors.to_csv(os.path.join(DATA_DIR, 'growth_key_youth_sectors.csv'), index = False)
+
+    wanted_youth_sectors = vacancies_by_sector.drop([0,1,3,4,5,6,7])
+    wanted_youth_sectors['Sector'] = wanted_youth_sectors['Sector'].str.wrap(25)
+    wanted_youth_sectors.to_csv(os.path.join(DATA_DIR, 'growth_wanted_youth_sectors.csv'), index = False)
 
 def summarise():
     monthly_vacancies = pd.read_csv(os.path.join(DATA_DIR, 'monthly_vacancies.csv'))
