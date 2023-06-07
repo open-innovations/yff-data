6f3b45aceb4a8c3eb57862b751e650f326ee7931
slowe <299787+slowe@users.noreply.github.com>
2023-06-07 13:01:54 +0100

    Remove transition time #191
    The "occasional" issue was due to a non-zero transition time.

--- src/assets/js/chart.js
+++ src/assets/js/chart.js
@@ -211,7 +211,7 @@
 			pad = 8;
 			if(typ=="bar-chart" || typ=="stacked-bar-chart") off = bb.height/2;
 
-			tip.setAttribute('style','position:absolute;left:'+(bb.left + bb.width/2 - bbo.left).toFixed(2)+'px;top:'+(bb.top + bb.height/2 - bbo.top).toFixed(2)+'px;display:'+(txt ? 'block':'none')+';z-index:10000;transform:translate3d(-50%,calc(-100% - '+off+'px),0);');
+			tip.setAttribute('style','position:absolute;left:'+(bb.left + bb.width/2 - bbo.left).toFixed(2)+'px;top:'+(bb.top + bb.height/2 - bbo.top).toFixed(2)+'px;display:'+(txt ? 'block':'none')+';z-index:10000;transform:translate3d(-50%,calc(-100% - '+off+'px),0);transition:all 0s;');
 			box.style.background = fill;
 			box.style.transform = 'none';
 			arr.style['border-top-color'] = fill;
