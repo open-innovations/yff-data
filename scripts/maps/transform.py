300e4a96715850bcaf64c0a1e5fd728eacdd5ba1
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2023-09-27 16:54:58 +0100

    Add combined unemployment data

--- scripts/maps/transform.py
+++ scripts/maps/transform.py
@@ -3,12 +3,14 @@ import pandas as pd
 
 WORKING_DIR = os.path.join('working', 'upstream')
 CONSTITUENCY_DATA = os.path.join(WORKING_DIR, 'labour-market_latest_by_pcon_2010.csv')
+COMBINED_DATA = os.path.join(WORKING_DIR, 'labour-market_most_recent_by_pcon_2010.csv')
 LOCAL_AUTHORITY_DATA = os.path.join(WORKING_DIR, 'labour-market_by_local_authority.csv')
 
 DATA_DIR = os.path.join('src', 'maps', 'employment', '_data')
 
 constituency_data = pd.read_csv(CONSTITUENCY_DATA)
 local_authority_data = pd.read_csv(LOCAL_AUTHORITY_DATA)
+combined_data = pd.read_csv(COMBINED_DATA)
 
 def filter_data(data, variable):
     return data.loc[
@@ -28,6 +30,17 @@ if __name__ == '__main__':
 
   unemployment_rate_20_24 = filter_data(constituency_data, 'Unemployment rate - aged 20-24')
   unemployment_rate_20_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_20_24.csv'), index=False)
+
+# Combined dates
+  unemployment_rate_16_24 = filter_data(combined_data, 'Unemployment rate - aged 16-64')
+  unemployment_rate_16_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_64_combined.csv'), index=False)
+
+  unemployment_rate_16_19 = filter_data(combined_data, 'Unemployment rate - aged 16-19')
+  unemployment_rate_16_19.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_19_combined.csv'), index=False)
+
+  unemployment_rate_20_24 = filter_data(combined_data, 'Unemployment rate - aged 20-24')
+  unemployment_rate_20_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_20_24_combined.csv'), index=False)
+
 
 # Local authority
   unemployment_rate_16_24 = filter_data(local_authority_data, 'Unemployment rate - aged 16-64')
