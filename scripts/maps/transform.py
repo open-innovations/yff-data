a68530b797fa72d46632d7411accd3fa5a843def
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2023-09-27 13:08:00 +0100

    Comment out local authority hexmaps

--- scripts/maps/transform.py
+++ scripts/maps/transform.py
@@ -2,11 +2,13 @@ import os
 import pandas as pd 
 
 WORKING_DIR = os.path.join('working', 'upstream')
-WORKING_DATA = os.path.join(WORKING_DIR, 'labour-market_latest_by_pcon_2010.csv')
+CONSTITUENCY_DATA = os.path.join(WORKING_DIR, 'labour-market_latest_by_pcon_2010.csv')
+LOCAL_AUTHORITY_DATA = os.path.join(WORKING_DIR, 'labour-market_by_local_authority.csv')
 
 DATA_DIR = os.path.join('src', 'maps', 'employment', '_data')
 
-all_data = pd.read_csv(WORKING_DATA)
+constituency_data = pd.read_csv(CONSTITUENCY_DATA)
+local_authority_data = pd.read_csv(LOCAL_AUTHORITY_DATA)
 
 def filter_data(data, variable):
     return data.loc[
@@ -17,12 +19,25 @@ def filter_data(data, variable):
 if __name__ == '__main__':
 
 
-# TODO: Update variable names
-  employment_rate_16_24 = filter_data(all_data, 'Unemployment rate - aged 16-64')
-  employment_rate_16_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_64.csv'), index=False)
+# Consituencies
+  unemployment_rate_16_24 = filter_data(constituency_data, 'Unemployment rate - aged 16-64')
+  unemployment_rate_16_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_64.csv'), index=False)
 
-  unemployment_rate_16_64 = filter_data(all_data, 'Unemployment rate - aged 16-19')
-  unemployment_rate_16_64.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_19.csv'), index=False)
+  unemployment_rate_16_19 = filter_data(constituency_data, 'Unemployment rate - aged 16-19')
+  unemployment_rate_16_19.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_19.csv'), index=False)
 
-  unemployment_rate_16_24 = filter_data(all_data, 'Unemployment rate - aged 20-24')
-  unemployment_rate_16_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_20_24.csv'), index=False)
+  unemployment_rate_20_24 = filter_data(constituency_data, 'Unemployment rate - aged 20-24')
+  unemployment_rate_20_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_20_24.csv'), index=False)
+
+# Local authority
+  unemployment_rate_16_24 = filter_data(local_authority_data, 'Unemployment rate - aged 16-64')
+  unemployment_rate_16_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_64_LA.csv'), index=False)
+
+  unemployment_rate_16_19 = filter_data(local_authority_data, 'Unemployment rate - aged 16-19')
+  unemployment_rate_16_19.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_16_19_LA.csv'), index=False)
+
+  unemployment_rate_20_24 = filter_data(local_authority_data, 'Unemployment rate - aged 20-24')
+  unemployment_rate_20_24.fillna(0).to_csv(os.path.join(DATA_DIR, 'unemployment_rate_20_24_LA.csv'), index=False)
+
+
+
