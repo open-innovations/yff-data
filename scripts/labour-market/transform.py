3fb561fea3fbc880538244086f9b0e62b2269c0e
Giles Dring <giles@dringtech.com>
2023-10-03 14:56:00 +0100

    Remove dependency on EDD for LMS data

--- scripts/labour-market/transform.py
+++ scripts/labour-market/transform.py
@@ -7,14 +7,14 @@ os.makedirs(DATA_DIR, exist_ok=True)
 
 if __name__ == '__main__':
     variables = column_mapper.keys()
-    lms_data = pd.read_csv('working/LMS_data.csv')
+    lms_data = pd.read_csv('working/upstream/lms.csv')
 
     extract = lms_data.loc[
         (lms_data.variable.isin(variables)) &
-        (lms_data['dates.freq'] == 'm')
+        (lms_data['freq'] == 'm')
     ]
 
-    extract = extract.drop(columns=['dates.freq']).rename(columns={'dates.date': 'lms_period'})
+    extract = extract.drop(columns=['freq']).rename(columns={'date': 'lms_period'})
     extract.lms_period = pd.to_datetime(extract.lms_period)
 
-    extract.loc[extract.lms_period >= '2000-01-01', :].to_csv(LMS_EXTRACT, index=False)
+    extract.loc[extract.lms_period >= '2000-01-01', ['variable', 'value', 'lms_period']].to_csv(LMS_EXTRACT, index=False)
--- scripts/vacancies/transform.py
+++ scripts/vacancies/transform.py
@@ -6,16 +6,14 @@ dataset_name = 'UK Vacancies (thousands) - Total'
 
 def transform_vacancies():
     lookup = pd.read_csv(os.path.join('working', 'lookups', 'LMS_variable_lookup.csv'), index_col=None)
-    data = pd.read_csv(os.path.join('working', 'LMS_data.csv'))
+    data = pd.read_csv(os.path.join('working', 'upstream', 'lms.csv'))
 
     code = lookup.query('name == @dataset_name').code.iloc[0]
     results = data.query('variable == @code').reset_index()
     results = results.drop(columns= ['index'], axis = 1).reset_index().rename(columns = {
                 'variable': 'code', 
-                'dates.date' : 'date',
-                'dates.freq' : 'freq'
                 })
-    results.to_csv(os.path.join(DATA_DIR, 'vacancies_edd.csv'), index = False)
+    results.loc[:, ['index', 'code', 'value', 'date', 'freq']].to_csv(os.path.join(DATA_DIR, 'vacancies_edd.csv'), index=False)
 
 
 if __name__ == "__main__":
