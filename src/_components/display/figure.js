00ea05edc6563a4cda86ca6433a4b45294cfb6f1
Giles Dring <giles@dringtech.com>
2023-10-10 14:34:03 +0100

    Update style for figure menu

--- src/_components/display/figure.js
+++ src/_components/display/figure.js
@@ -3,8 +3,8 @@ export default function ({ content, id, source_file }) {
     const idProp = id ? ` id=${id}` : '';
     const fileLink = source_file ? `
       <a href="${ source_file }" role="menuitem" class="figure-option orange-bg">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-spreadsheet-fill" viewBox="0 0 16 16"><path d="M6 12v-2h3v2H6z"/><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM3 9h10v1h-3v2h3v1h-3v2H9v-2H6v2H5v-2H3v-1h2v-2H3V9z"/></svg>
-      Get data
+        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-file-earmark-spreadsheet-fill" viewBox="0 0 16 16"><path d="M6 12v-2h3v2H6z"/><path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM3 9h10v1h-3v2h3v1h-3v2H9v-2H6v2H5v-2H3v-1h2v-2H3V9z"/></svg>
+        <span>Get data</span>
       </a>` : ''
 
     return `<div class='pane'${idProp}>
