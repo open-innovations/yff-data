ca9b9c6a84053cd39f29abb35b4ca3f0d21725db
Giles Dring <giles@dringtech.com>
2023-12-11 15:47:15 +0000

    Fix colour scales for NEET

--- src/maps/neet-factors/_data.ts
+++ src/maps/neet-factors/_data.ts
@@ -1,10 +1,10 @@
 
 export const legendKey = (_: number, n: number) => {
   return [
-    'least likely',
-    '',
-    '',
-    '',
     'most likely',
+    '',
+    '',
+    '',
+    'least likely',
   ][n]
 }
