d5b3a987d1e1c4e1ad39a9a571b9f8c92d58ba5c
slowe <299787+slowe@users.noreply.github.com>
2024-02-08 19:05:53 +0000

    Build KS4 characteristic outcome file

--- scripts/dfe/transform_char_outcomes.py
+++ scripts/dfe/transform_char_outcomes.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pandas as pd 
 
 WORKING_DIR = os.path.join('working', 'upstream')
@@ -10,26 +11,56 @@ char_outcomes_data = pd.read_csv(KS4_CHAR_OUTCOMES_DATA)
 
 fields = ['new_la_code', 'time_period', 'version', 'gender', 'breakdown', 'ethnicity_major', 
           'free_school_meals', 'sen_status', 'sen_description', 'disadvantage', 'first_language', 'variable', 'value']
+groupby = ['variable','breakdown','gender','ethnicity_major','free_school_meals','sen_status','sen_description','disadvantage','first_language']
 
-ATT8_DIR = os.path.join('src', 'maps', 'education', '_data', 'view', 'ks4_char_outcomes', 'avg_att8')
-os.makedirs(ATT8_DIR, exist_ok=True)
+DATA_DIR = os.path.join('src', 'maps', 'education', '_data', 'view');
+os.makedirs(DATA_DIR, exist_ok=True)
 
-P8_DIR = os.path.join('src', 'maps', 'education', '_data', 'view', 'ks4_char_outcomes', 'avg_p8')
-os.makedirs(P8_DIR, exist_ok=True)
 
-def filter_data(data, variable, fields, filter_field):
-    return data.loc[data[filter_field] == variable, fields]
+def save_tidy_csv(file,df):
+    # First add the header
+    ncol = len(df.columns)
+    new_record = pd.DataFrame([[*['---'] * ncol]],columns=df.columns)
+    final = pd.concat([new_record, df], ignore_index=True)
 
-def save_to_file(data, filename, DATA_DIR):
-    full_filename = os.path.join(DATA_DIR, filename)
-    data.to_csv(full_filename, index=False)
-    return data
+    # Get the output as CSV
+    csv = final.to_csv(index=True)
 
-def clean_nulls(data):
-    return data.dropna()
+    # Because we added an index column we will now tidy 
+    csv = re.sub(r'\n0,---,---', '\n---,---,---', csv)
 
+    text_file = open(os.path.join(DATA_DIR, 'ks4_char_outcomes.csv'), "w")
+    text_file.write(csv)
+    text_file.close()
+
+def add_header_end(df):
+    ncol = len(df.columns)
+    new_record = pd.DataFrame([[*['---'] * ncol]],columns=df.columns)
+    df = pd.concat([new_record, df], ignore_index=True)
+    return df
 
 if __name__ == '__main__':
+
+    # Filter to the allowed fields
+    filtered = char_outcomes_data[fields].dropna()
+
+    # Limit to rows that match 202223 and Revised
+    limited = filtered.loc[(filtered['time_period']==202223) & (filtered['version']=='Revised') & (filtered['breakdown']!='Ethnicity minor')]
+
+    # Pivot the table to arrange in columns with each combo
+    pivotted = limited.pivot_table(index='new_la_code', columns=groupby, values='value');
+
+    # Add a column at the start which is a duplicate of the index (so we can not print the index column)
+    pivotted.insert(0,'LADCD',pivotted.index)
+
+    save_tidy_csv(os.path.join(DATA_DIR, 'ks4_char_outcomes.csv'),pivotted);
+
+
+    exit()
+
+
+
+
 
     char_outcomes_data = char_outcomes_data.dropna().drop(columns= [
         'education_investment_area_flag',
--- scripts/dfe/transform_ks4_outcomes.py
+++ scripts/dfe/transform_ks4_outcomes.py
@@ -1,4 +1,5 @@
-import os 
+import os
+import re
 import pandas as pd
 
 WORKING_DIR = os.path.join('working', 'upstream')
@@ -9,16 +10,27 @@ KS4_OUTCOMES_DATA = os.path.join(
 ks4_outcomes_data = pd.read_csv(KS4_OUTCOMES_DATA)
 
 fields = ['new_la_code', 'time_period', 'version', 'gender', 'variable', 'value']
+groupby = ['variable','gender']
 
 DATA_DIR = os.path.join('src', 'maps', 'education', '_data', 'view');
 os.makedirs(DATA_DIR, exist_ok=True)
 
-
-def add_header_end(df):
+def save_tidy_csv(file,df):
+    # First add the header
     ncol = len(df.columns)
     new_record = pd.DataFrame([[*['---'] * ncol]],columns=df.columns)
-    df = pd.concat([new_record, df], ignore_index=True)
-    return df
+    final = pd.concat([new_record, df], ignore_index=True)
+
+    # Get the output as CSV
+    csv = final.to_csv(index=True)
+
+    # Because we added an index column we will now tidy 
+    csv = re.sub(r'\n0,---,---', '\n---,---,---', csv)
+
+    text_file = open(os.path.join(DATA_DIR, 'ks4_outcomes.csv'), "w")
+    text_file.write(csv)
+    text_file.close()
+
 
 if __name__ == '__main__':
 
@@ -29,14 +41,11 @@ if __name__ == '__main__':
     limited = filtered.loc[(filtered['time_period']==202223) & (filtered['version']=='Revised')]
 
     # Pivot the table so that we have columns for variable+gender
-    pivotted = limited.pivot_table(index='new_la_code', columns=['variable','gender'], values='value');
+    pivotted = limited.pivot_table(index='new_la_code', columns=groupby, values='value');
 
     # Add a column at the start which is a duplicate of the index (so we can not print the index column)
     pivotted.insert(0,'LADCD',pivotted.index)
 
-    # Add a row of dashes
-    final = add_header_end(pivotted)
-
     # Need to add a separator row
-    final.to_csv(os.path.join(DATA_DIR, 'ks4_outcomes.csv'),index=False)
+    save_tidy_csv(os.path.join(DATA_DIR, 'ks4_outcomes.csv'),pivotted)
     
