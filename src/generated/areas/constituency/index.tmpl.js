05c2b242ecf785353d4e28eb17a7d3c6cc00968e
slowe <299787+slowe@users.noreply.github.com>
2024-02-22 16:57:57 +0000

    Moving constituencies

--- src/generated/areas/constituency/index.tmpl.js
+++ src/generated/areas/constituency/index.tmpl.js
@@ -2,19 +2,21 @@ export const layout = 'layouts/areas/pcon.njk';
 
 export const tags = ['constituency'];
 
-export default function*({ areas }) {
+export default function*({ areas, page }) {
   // Iterate over all the areas
   for (const area of areas.reference.pcon) {
     // Read the keys out of the area and map to more friendly names
     const { PCON22NM: name, PCON22CD: code } = area;
-
-    // Yield the data which creates the page
+	
+	// Yield the data which creates the page
     yield {
       title: name,
       url: `/areas/constituency/${ code }/`,
       area: {
         code: code,
         type: 'PCON22',
+		summary: page.data[code].summary||{},
+		map: page.data[code].map||{}
       }
     }
   }
