e080d735dfbab8e5c4debe5b112a4cd1ba3b025f
Taz-Chiles <97239140+Taz-Chiles@users.noreply.github.com>
2024-02-23 14:45:44 +0000

    Add NEET data to international dashboard

--- scripts/international/prepare.py
+++ scripts/international/prepare.py
@@ -4,11 +4,17 @@ import os
 WORKING_DIR = os.path.join('working', 'upstream')
 OECD_LFS_DATA = os.path.join(
     WORKING_DIR, 'oecd_lfs_by_sex_and_age.csv')
+OECD_NEET_DATA = os.path.join(
+    WORKING_DIR, 'oecd-neet.csv'
+)
 
 DATA_DIR = os.path.join('src', 'maps', 'international', '_data', 'view')
 os.makedirs(DATA_DIR, exist_ok=True)
 
 oecd_lfs_data = pd.read_csv(OECD_LFS_DATA).round(1)
+oecd_neet_data = pd.read_csv(OECD_NEET_DATA).round(1)
+
+
 
 def filter_data(data, variable, filter_field):
     return data.loc[data[filter_field] == variable]
@@ -95,11 +101,37 @@ if __name__ == '__main__':
                 filter_data, '15 years or over', 'age').pipe(
                     save_to_file, 'economic_inactivity_rate_male.csv') 
 
-
+# PREPARE NEET DATA
     
 
+    neet_15_19_all = filter_data(oecd_neet_data, '15_19', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_19_all.csv')
 
+    neet_15_19_women = filter_data(oecd_neet_data, '15_19_WOMEN', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_19_women.csv')
+
+    neet_15_19_men = filter_data(oecd_neet_data, '15_19_MEN', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_19_men.csv')
+
+    neet_20_24_all = filter_data(oecd_neet_data, '20_24', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_20_24_all.csv')
     
+    neet_20_24_women = filter_data(oecd_neet_data, '20_24_WOMEN', 'age_range').pipe(
+    filter_data, 2022, 'time').pipe(save_to_file, 'neet_20_24_women.csv')
+
+    neet_20_24_men = filter_data(oecd_neet_data, '20_24_MEN', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_20_24_men.csv')
+    
+    neet_15_29_all = filter_data(oecd_neet_data, '15_29', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_29_all.csv')
+    
+    neet_15_29_women = filter_data(oecd_neet_data, '15_29_WOMEN', 'age_range').pipe(
+    filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_29_women.csv')
+
+    neet_15_29_men = filter_data(oecd_neet_data, '15_29_MEN', 'age_range').pipe(
+        filter_data, 2022, 'time').pipe(save_to_file, 'neet_15_29_men.csv')
+
+
     
     
 
